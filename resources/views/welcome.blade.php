db3297f8c92e0dcd52c04b741e6a1ae6d48bcca6
Imam Tashbir <imamtashbir95@gmail.com>
2024-06-11 02:59:08 +0700

    Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + React</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite('resources/js/frontend/src/index.jsx')
 </head>
 <body>
