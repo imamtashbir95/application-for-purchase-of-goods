3c8277ff0dc750dab50cfbfd729d3cb06375bab7
Imam Tashbir <imamtashbir95@gmail.com>
2024-06-10 15:36:35 +0700

    Commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/frontend/src/index.jsx'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/frontend/src/index.jsx'
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            external: ['bootstrap/dist/css/bootstrap.min.css'],
+        }
+    }
 });
