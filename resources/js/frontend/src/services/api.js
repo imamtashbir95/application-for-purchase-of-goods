9243c627c1377059c58ded781c81ca45b0234e85
Imam Tashbir <imamtashbir95@gmail.com>
2024-06-11 04:51:19 +0700

    Commit

--- resources/js/frontend/src/services/api.js
+++ resources/js/frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: '/api',
     // headers: {
     //     'Authorization': `Bearer ${token}`
     // },
